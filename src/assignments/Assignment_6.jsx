2715e5443c37f14c33ce5f8c0ed905e7e47196b4
Yevan Dewathanthriarachchi <yevand2006@gmail.com>
2025-07-31 17:31:04 +0530

    calculator final

--- src/assignments/Assignment_6.jsx
+++ src/assignments/Assignment_6.jsx
@@ -3,19 +3,20 @@ import { useState } from "react";
 
 
 export default function Assignment_6() {
-    const [style, setStyle] = useState([]);
-    const updateStyle = () => {
-        setStyle()
-    }
+    // const [style, setStyle] = useState([]);
+    // const updateStyle = () => {
+    //     setStyle()
+    // }
 
     return (
         <div>
-            <h4 style={style}>CHANGE MY STYLE</h4>
+            <h3>Coming soon</h3>
+            {/* <h4 style={style}>CHANGE MY STYLE</h4>
             <label htmlFor="name">Enter rule name</label> <input type="text" id="name" />
             <br />
             <label htmlFor="value">Enter rule value</label> <input type="text" id="value" />
             <br /><br />
-            <button onClick={updateStyle}>Add</button>
+            <button onClick={updateStyle}>Add</button> */}
 
         </div>
     )
