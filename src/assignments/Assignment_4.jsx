c2b805b3f2a53a32405889bb894915d192b940ab
Yevan Dewathanthriarachchi <yevand2006@gmail.com>
2025-08-01 14:12:22 +0530

    Assignment 4 remade

--- src/assignments/Assignment_4.jsx
+++ src/assignments/Assignment_4.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-export default function Assignment_4(){
-const [input, setInput] = useState("");
-    const [total, setTotal] = useState(0);
-    const [average, setAverage] = useState("");
+export default function Assignment_4() {
+    const [input, setInput] = useState("");
     const [items, setItems] = useState([]);
 
     const addItems = () => {
@@ -18,14 +16,25 @@ const [input, setInput] = useState("");
         }
     };
 
+    const deleteItems = (indexToDel) => {
+        const updated = items.filter((_, i) => i !== indexToDel);
+        setItems(updated)
+    }
+
     return (
         <div>
-            <ul></ul>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index} style={{margin: "10px 0"}}>
+                        {item}  <button onClick={() => deleteItems(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
             <input
-             type="number"
-             value={input}
-             onChange={(e) => setInput(e.target.value)}
-             />
+                type="number"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
             <br />  <br />
             <button onClick={addItems}>Add</button>
         </div>
