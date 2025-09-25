1d546f76949dfe9d86581e7219e4a736a7e7a31b
Yevan Dewathanthriarachchi <yevand2006@gmail.com>
2025-09-25 21:21:47 +0530

    Scrollable video

--- dist/assets/Assignment_28-BE2_0kKK.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-D_j8DqGd.js";function s(){return n.jsx("div",{children:"coming soon"})}export{s as default};
--- src/assignments/Assignment_28.jsx
+++ src/assignments/Assignment_28.jsx
@@ -1,89 +1,82 @@
 import { useEffect, useRef, useState } from "react";
 import './Assignment_28.css';
-import { div, video } from "framer-motion/client";
 
 
 export default function Assignment_28() {
-    // const [sections, setSections] = useState([]);
-    // const videoRef = useRef(null);
-    // const [loaded, setLoaded] = useState(false);
-    // const scrollref = useRef(0);
+    const [sections, setSections] = useState([]);
+    const videoRef = useRef(null);
 
 
-    // useEffect(() => {
-    //     setSections([
-    //         'Section 1',
-    //         'Section 2',
-    //         'Section 3',
-    //         'Section 4',
-    //         'Section 5',
-    //         'Section 6',
-    //         'Section 7',
-    //         'Section 8',
-    //         'Section 9',
-    //         'Section 10'
-    //     ])
-    // }, [])
+    useEffect(() => {
+        setSections(
+            [
+                'Section 1',
+                'Section 2',
+                'Section 3',
+                'Section 4',
+                'Section 5',
+                'Section 6',
+                'Section 7',
+                'Section 8',
+                'Section 9',
+                'Section 10'
+            ]
+        )
+    }, [])
 
-    // useEffect(() => {
-    //     const video = videoRef.current
-    //     setLoaded(false);
-    //     const playBack = 50;
+    useEffect(() => {
+        const video = videoRef.current;
+        let ticking = false;
 
-    //     const handlePlay = () => {
-    //         if (video) {
-    //             const frameNumber = window.pageYOffset / playBack;
-    //             video.currentTime = frameNumber;
-    //         }
-    //         window.requestAnimationFrame(handlePlay);
-    //     }
-    //     window.requestAnimationFrame(handlePlay);
-    // }, [])
 
-    // useEffect(() => {
-    //     const video = videoRef.current;
+        window.onscroll = () => {
+            if (!video || !video.duration) return;
 
-    //     video.addEventListener('loadmetadata', () => {
-    //         const { duration } = video;
-    //         const playBack = 50;
-    //         const scrollSection = scrollref.current;
-    //         if (videoRef?.current) {
-    //             scrollSection.style.height = Math.floor(duration) * playBack + 'px'
-    //         }
-    //     })
-    // }, [])
-    // return (
-    //     <div>
-    //         <div
-    //             className="background"
-    //         >
-    //             <video
-    //                 ref={videoRef}
-    //                 src="/scroll video.mp4"
-    //             >
-    //             </video>
-    //         </div>
+            if (!ticking) {
+                window.requestAnimationFrame(() => {
+                    const scrollTop = window.scrollY;
+                    const scrollHeight = document.body.scrollHeight - window.innerHeight;
+                    const scrollFraction = scrollTop / scrollHeight;
 
-    //         <div
-    //             className="content"
-    //             ref={scrollref}
-    //         >
+                    video.currentTime = scrollFraction * video.duration;
+                    ticking = false;
+                });
+                ticking = true;
+            }
+        };
+    }, [])
 
-    //             {sections.map((section, sectionIndex) => (
-    //                 <div
-    //                     className="text"
-    //                     key={sectionIndex}
-    //                 >
-    //                     {section}
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     </div>
-    // )
-
-    return(
+    return (
         <div>
-            coming soon
+            <div>
+
+                {sections.map((section, sectionIndex) => (
+                    <div
+                        className="text"
+                        key={sectionIndex}
+                    >
+                        {section}
+                    </div>
+                ))}
+            </div>
+            
+            <div
+                className="background"
+            >
+                <div
+                    className="video">
+                    <video
+                        ref={videoRef}
+                        src="/scroll video.mp4"
+                        muted
+                        preload="auto"
+                        playsInline
+                    >
+                    </video>
+                </div>
+            </div>
+
+
         </div>
     )
 }
