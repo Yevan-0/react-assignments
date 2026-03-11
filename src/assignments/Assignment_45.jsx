36ccca43d1f0dc76e03627e06bc8bfa9f56651e7
Yevan Dewathanthriarachchi <yevand2006@gmail.com>
2026-03-11 23:53:11 +0530

    Assignment_45(final): Implemented the detection model and detect button

--- src/assignments/Assignment_45.jsx
+++ src/assignments/Assignment_45.jsx
@@ -1,11 +1,15 @@
 import { useState, useRef } from "react";
 import "./Assignment_45.css";
+import * as blazeface from "@tensorflow-models/blazeface";
+import "@tensorflow/tfjs"; 
 
 export default function Assignment_45() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [error, setError] = useState('');
   const [source, setSource] = useState(null);
   const fileInputRef = useRef(null);
+  const [faces, setFaces] = useState([]);
+  const imgRef = useRef(null);
 
   const changeFile = (e) => {
     const file = e.target.files?.[0];
@@ -20,9 +24,24 @@ export default function Assignment_45() {
   };
 
   const handleUpload = () => {
-    fileInputRef.current.value = ""; 
+    fileInputRef.current.value = "";
     fileInputRef.current.click();
   };
+
+  const handleDetect = async () => {
+    if (!imgRef.current) return;
+
+    try {
+      const model = await blazeface.load();
+      const predictions = await model.estimateFaces(imgRef.current, false);
+
+      console.log("Predictions:", predictions);
+      setFaces(predictions);
+    } catch (err) {
+      setError("Face detection Failed" + err.message);
+      console.log(err)
+    }
+  }
 
   return (
     <div>
@@ -52,6 +71,7 @@ export default function Assignment_45() {
 
           <button
             className="detect-btn"
+            onClick={handleDetect}
           >
             Detect
           </button>
@@ -59,7 +79,32 @@ export default function Assignment_45() {
 
         <div className="cont-bottom">
           {error && <p style={{ color: "red" }}>{error}</p>}
-          {source && <img src={source} alt="preview" />}
+          {source && (
+            <div style={{ position: "relative" }}>
+              <img ref={imgRef} src={source} alt="preview" />
+              {faces.map((face, i) => {
+                const [x, y, width, height] = [
+                  face.topLeft[0],
+                  face.topLeft[1],
+                  face.bottomRight[0] - face.topLeft[0],
+                  face.bottomRight[1] - face.topLeft[1],
+                ];
+                return (
+                  <div
+                    key={i}
+                    style={{
+                      position: "absolute",
+                      border: "3px solid red",
+                      left: x,
+                      top: y,
+                      width: width,
+                      height: height,
+                    }}
+                  />
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
